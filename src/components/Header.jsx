125aa6aefd90d0f563a669d5ef87787eb983e33b
Bhavanapatil2911 <patilbhavana2911@gmail.com>
2024-12-22 17:42:48 +0530

    Home page updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ const Header = () => {
   return (
     <header>
       <nav>
+        <img src="../assets/AGNLogo.png" alt="" />
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
