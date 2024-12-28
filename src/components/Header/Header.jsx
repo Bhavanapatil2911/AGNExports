717c6a46c49c2853c583a8f632ca9f0936a14cf4
Bhavanapatil2911 <patilbhavana2911@gmail.com>
2024-12-28 22:17:29 +0530

    fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./header.css";
-import MainLogo from "../../images/MainLogo.png";
+import MainLogo from "../../images/AGN-Photoroom.png";
 
 const Header = ({ isSticky }) => {
   return (
