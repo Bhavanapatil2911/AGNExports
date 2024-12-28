3b844dc73006549108f1f3217158e5c5975412a4
Bhavanapatil2911 <patilbhavana2911@gmail.com>
2024-12-28 13:06:25 +0530

    fixed

--- src/components/Banner/banner.jsx
+++ src/components/Banner/banner.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import './banner.css';
+import bannerImage from '../../images/rice-field-1866498_1920.jpg';
 
 const Banner = () => {
   return (
-    <section className="banner">
+    <section
+      className="banner"
+      style={{
+        backgroundImage: `url(${bannerImage})`,
+      }}
+    >
       <div className="banner-content align-self-end mb-5">
         <button className="explore-btn rounded-pill mb-4">Explore</button>
       </div>
     </section>
   );
 };
+
 
 export default Banner;
