71699d4f4c566a165cb463441a52779272fd44c0
Bhavanapatil2911 <patilbhavana2911@gmail.com>
2024-12-28 13:45:55 +0530

    “fixed”

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import "./footer.css";
+
+import footerimage from '../../images/footerBg.jpg';
 
 const Footer = () => {
   return (
-    <footer>
+    <footer 
+      className="footer"
+      style={{
+      backgroundImage: `url(${footerimage})`,
+    }}
+    >
+      <div className="row">
+
+      </div>
       <p>© 2024 Your Website. All rights reserved.</p>
     </footer>
   );
