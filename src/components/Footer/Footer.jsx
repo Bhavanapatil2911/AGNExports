1df19058098ea912d56b034ccb2e3ead06e5e809
Bhavanapatil2911 <patilbhavana2911@gmail.com>
2024-12-29 22:51:52 +0530

    “fixed”

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,15 +12,16 @@ const Footer = () => {
         backgroundImage: `url(${footerimage})`,
       }}
     >
-      <div className="container">
-        <div className="row">
-          {/* Empty first column */}
+      <div className="container-lg">
+        <div className="row justify-content-around ">
           <div className="col-md-4">
-            {/* Empty */}
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3844.8866305278975!2d78.48183157452837!3d15.490529085108223!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bb5ad5064dcd90d%3A0x665b1eda6d6198cc!2sAGN%20jewellers!5e0!3m2!1sen!2sin!4v1735492460835!5m2!1sen!2sin"
+             width="500" height="450" style={{border: "0"}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
           </div>
 
           {/* Second column with Quick Links */}
-          <div className="col-md-4">
+          <div className="col-md-3">
             <h4>Quick Links</h4>
             <ul>
               <li>
